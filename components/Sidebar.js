595cda65acda240a757493552cdf3bceacd2c3d5
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-23 23:44:02 -0500

    update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,13 +3,15 @@ import styled from 'styled-components'
 function Sidebar() {
     return (
         <Container>
-            Mood
+            <Header>
+            
+            </Header>
         </Container>
     )
 }
 
 export default Sidebar;
 
-const Container = styled.div`
+const Container = styled.div``;
 
-`;
+const Header = styled.div``;
