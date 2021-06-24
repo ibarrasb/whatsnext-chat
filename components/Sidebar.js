ae2a81e15637e8b22fbb3a6b92f94d8ae08615ca
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-23 23:43:23 -0500

    next.js app initiated, installed dependencies, implementing sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function Sidebar() {
+    return (
+        <Container>
+            Mood
+        </Container>
+    )
+}
+
+export default Sidebar;
+
+const Container = styled.div`
+
+`;
