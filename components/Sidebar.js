a48439c62c13058c7ee3a668ce27d6432fa76795
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-25 12:07:38 -0500

    styled components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,17 +1,39 @@
+import { Avatar, IconButton } from '@material-ui/core';
 import styled from 'styled-components'
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 
 function Sidebar() {
     return (
         <Container>
             <Header>
-            
+                <IconsContainer>
+
+                    <UserAvatar/>
+
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+
+                </IconsContainer>
             </Header>
         </Container>
     )
 }
+
+
 
 export default Sidebar;
 
 const Container = styled.div``;
 
 const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
