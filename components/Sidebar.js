bc3dafd955dc43107d8f8ba36e4a109bd22d5673
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-30 16:49:36 -0500

    firebase set up for google authentication, more styled components added

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,27 +1,48 @@
-import { Avatar, IconButton } from '@material-ui/core';
+import { Avatar, IconButton, Button } from '@material-ui/core';
 import styled from 'styled-components'
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search'
 
 
 function Sidebar() {
+
+    const createChat = () => {
+        const input = alert(
+        "Please Enter a email address for a user you would like to chat with"
+        )
+        if(!input) return null;
+    }
+
     return (
         <Container>
             <Header>
-                <IconsContainer>
+                 <UserAvatar/>
 
-                    <UserAvatar/>
+                <IconsContainer>
 
                     <IconButton>
                         <ChatIcon/>
                     </IconButton>
-                
+
                     <IconButton>
                         <MoreVertIcon/>
                     </IconButton>
 
                 </IconsContainer>
             </Header>
+            <Search>
+                <SearchIcon/>
+                <SearchInput placeholder="Search in chat"/>
+            </Search>
+
+            <SidebarButton onClick={createChat}>
+                Start a new chat
+            </SidebarButton>
+
+    {/* List of chats */}
+
+
         </Container>
     )
 }
@@ -32,8 +53,47 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 2px;
+`;
 
-const UserAvatar = styled(Avatar)``;
+const SidebarButton = styled(Button)`
+    width: 100%;
+
+    &&&{
+        border-top: 1px solid whitesmoke;
+        border-bottom: 1px solid whitesmoke;
+    }
+`;
+
+const SearchInput = styled.input`
+    outline-width: 0;
+    border: none;
+    flex: 1;
+`;
+
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+    
+    :hover {
+        opacity: 0.8;
+    }
+`;
 
 const IconsContainer = styled.div``;
---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBx-aWb4ST_xmy_o_YqY3NXfWYhoh_W3jg",
+    authDomain: "whatsnext-v1.firebaseapp.com",
+    projectId: "whatsnext-v1",
+    storageBucket: "whatsnext-v1.appspot.com",
+    messagingSenderId: "237962981731",
+    appId: "1:237962981731:web:ef09f4223468f4b70e6356"
+  };
+
+const app = !firebase.apps.length 
+? firebase.initializeApp(firebaseConfig) 
+: firebase.app();
+
+const db = firebase.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {db, auth, provider};
